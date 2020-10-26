6bde8cf75883310797086f26d0f15c5f53b8a15b
Nelson <nbritob@hotmail.com>
2020-10-26 10:19:29 +0000

    adds app.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Cards, Chart, CountryPicker } from "./components";
+import styles from './App.module.css';
 
 function App() {
   return (
-    <div>
+    <div className={styles.container}>
       <Cards />
       <CountryPicker />
       <Chart />
