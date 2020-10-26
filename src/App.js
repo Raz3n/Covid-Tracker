0be1aeb9b05f8e25841d77d6b46d440e96511574
Nelson <nbritob@hotmail.com>
2020-10-26 10:39:41 +0000

    adds fetchData, going with destructured approach to see how it works

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, {useEffect} from "react";
+
 import { Cards, Chart, CountryPicker } from "./components";
 import styles from './App.module.css';
 
+import {fetchData} from './api/index'
+
+
 function App() {
+
+  useEffect(async() => {
+    const data = await fetchData();
+    console.log(data)
+  })
+  
   return (
     <div className={styles.container}>
       <Cards />
