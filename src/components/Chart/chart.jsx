8076c804044b4ed4ab990233cf94a8be922b01b1
Nelson <nbritob@hotmail.com>
2020-10-27 02:08:39 +0000

    fixes missing dependency issue

--- src/components/Chart/chart.jsx
+++ src/components/Chart/chart.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { fetchDailyData } from "../../api";
-// import {Line, Bar} from 'react-chartjs-2';
+import {Line, Bar} from 'react-chartjs-2';
+
+import styles from './chart.module.css';
+
 
 const Chart = () => {
   const [dailyData, setDailyData] = useState({});
