89b106ea31600447701eb9314f394d4e8828ad1a
Nelson <nbritob@hotmail.com>
2020-10-30 00:37:33 +0000

    name change from global to US as API had to be changed

--- src/components/CountryPicker/countryPicker.jsx
+++ src/components/CountryPicker/countryPicker.jsx
@@ -21,7 +21,7 @@ const CountryPicker = ({ handleCountryChange }) => {
         defaultValue=""
         onChange={(e) => handleCountryChange(e.target.value)}
       >
-        <option value="">Global</option>
+        <option value="">US</option>
         {fetchedCountries.map((country, i) => (
           <option key={i} value={country}>
             {country}
