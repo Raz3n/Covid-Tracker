a4ee699120b80578a7882d6b6de3d7d222dd1070
Nelson <nbritob@hotmail.com>
2020-10-30 22:34:19 +0000

    adds & Global

--- src/components/CountryPicker/countryPicker.jsx
+++ src/components/CountryPicker/countryPicker.jsx
@@ -21,7 +21,7 @@ const CountryPicker = ({ handleCountryChange }) => {
         defaultValue=""
         onChange={(e) => handleCountryChange(e.target.value)}
       >
-        <option value="">US</option>
+        <option value="">US & Global</option>
         {fetchedCountries.map((country, i) => (
           <option key={i} value={country}>
             {country}
